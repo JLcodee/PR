b0dd3a6c1eee430055d9f00d8448034ca82114b4
JLcodee <147172155+JLcodee@users.noreply.github.com>
2024-09-13 10:45:47 +0700

    Add files via upload

---
+++ newprog5/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="signup-form-form">
+    <form class="signup-form" action="index.inc.php" method="POST">
+        <input type="text" name="name" placeholder= "Full Name">
+        <input type="text" name="lrn" placeholder= "LRN">
+        <input type="text" name="pwd" placeholder= "Password">
+        <input type="text" name="pwdrepeat" placeholder= "Repeat Password">
+        <button type="submit">SUBMIT</button>
+    </form>
+    </div>
+</body>
+</html>
